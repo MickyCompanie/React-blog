e937fd56e8838ae42f30575c3ac15b883198cb0b
MickyCompanie <micky.companie@gmail.com>
2021-06-01 15:39:00 +0200

    dotenv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const mongoose = require('mongoose');
 mongoose.connect(
-    'mongodb+srv://Micky:ynq803P7@react-blog.wegoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'mongodb+srv://Micky:'+ process.env.MONGO_ATLAS_PASSWORD + '@react-blog.wegoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
     { useNewUrlParser: true } ).then( () => console.log('Db Connected')) // prevent deprecation warning from mangoose
                                 .catch(err => console.log(err))
 
